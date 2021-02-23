299e7ef82845cae1ff2ba0551c215016fab56ce1
JakinChan200 <42459094+JakinChan200@users.noreply.github.com>
2021-02-22 21:45:12 -0800

    Added lab2 ex1

---
+++ Lab 2/lab2_ex1.asm
@@ -0,0 +1,20 @@
+;=================================================
+; Name: Jakin Chan
+; Email: jchan419@ucr.edu
+; 
+; Lab: lab 2. ex 1
+; Lab section: 22
+; TA: Jason Zellmer
+; 
+;=================================================
+
+.orig x3000
+
+	LD R3, DEC_65
+	LD R4, HEX_41
+HALT
+
+	DEC_65 .FILL #65
+	HEX_41 .FILL x41
+
+.end
