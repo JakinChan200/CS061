cd23ba7177828e4b402dc73494bc9a55c42080d9
JakinChan200 <42459094+JakinChan200@users.noreply.github.com>
2021-02-26 17:19:23 -0800

    Added lab4 ex3

---
+++ Labs/Lab 4/lab4_ex3.asm
@@ -0,0 +1,35 @@
+;=================================================
+; Name: Jakin Chan
+; Email: jchan419@ucr.edu
+; 
+; Lab: lab 4, ex 3
+; Lab section: 22
+; TA: Jason Zellmer
+; 
+;=================================================
+.orig x3000
+	
+	LD R1, POINTER			
+	LD R2, COUNTER
+	LD R3, ONE
+	
+	LOOP
+		STR R3, R1, #0			;Store value into address in R1
+		ADD R3, R3, R3			;Increment value
+		ADD R1, R1, #1			;Increment address
+		ADD R2, R2, #-1			;Decrement counter
+		BRp LOOP
+	END_LOOP
+	
+	LD R1, POINTER				;Reset R1 to point to head of array
+	LDR R2, R1, #6				;Get 6th value from head and put in R2
+	
+
+HALT
+	POINTER .FILL x4000
+	COUNTER .FILL #10
+	ONE		.FILL x1
+	.orig x4000
+	.BLKW #10
+
+.end
